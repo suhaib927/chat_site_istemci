059824dee366653b20a76a1c50e6210a30b59f01
suhaib <suhaib@DESKTOP-P6DGD12>
2024-12-11 16:59:44 +0300

    Add project files.

---
+++ istemci/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace istemci.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
