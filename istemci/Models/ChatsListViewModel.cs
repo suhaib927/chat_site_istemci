751c1c968b011165a8cec788c63621db81ec34af
SUHAIB OTHMAN <suhaibalhajothman@gmail.com>
2024-12-29 05:11:35 +0300

    last update

--- istemci/Models/ChatsListViewModel.cs
+++ istemci/Models/ChatsListViewModel.cs
@@ -6,5 +6,7 @@ namespace chat_site_istemci.Models
     {
         public List<User> users { get; set; }
         public List<Group> groups { get; set; }
+        public string Broadcast { get; set; } = "c977d8ac-691c-41b3-8935-dcf2b0f68c86";
+
     }
 }
